d012c4c16abef8ae26083cd83c81e3e5296f5e1d
hsinpa <hsinpa@gmail.com>
2020-03-06 23:21:59 +0800

    no message

--- Assets/VoiceNetwork/Script/VoiceNetworkPlayer.cs
+++ Assets/VoiceNetwork/Script/VoiceNetworkPlayer.cs
@@ -14,7 +14,7 @@ namespace VoiceNetwork
         [SerializeField]
         public AudioSource playoutAudioSource;
 
-        private bool isMicAvailable;
+        private string microphoneName = "";
 
         public override void OnStartClient()
         {
@@ -22,6 +22,11 @@ namespace VoiceNetwork
             playoutAudioSource.clip = AudioClip.Create("Receiver", 1024, 1, 44100, false);
 
             string[] devices = Microphone.devices;
+
+            if (devices != null && devices.Length > 0) {
+                microphoneName = devices[0];
+                micAudioSource.clip = Microphone.Start(microphoneName, true, 1, 44100);
+            }
 
             isMicAvailable = (devices != null && devices.Length > 0);
         }
@@ -34,16 +39,19 @@ namespace VoiceNetwork
             System.Buffer.BlockCopy(raw_data, 0, byteArray, 0, byteArray.Length);
 
             if (raw_data != null && raw_data.Length > 0)
-                RpcReceiveAudio(raw_data);
+                RpcReceiveAudio(byteArray);
         }
 
         [ClientRpc]
-        public void RpcReceiveAudio(float[] raw_data)
+        public void RpcReceiveAudio(byte[] raw_data)
         {
-            Debug.Log(raw_data.Length);
+            if (!isLocalPlayer) {
+                var floatArray2 = new float[raw_data.Length / 4];
+                System.Buffer.BlockCopy(raw_data, 0, floatArray2, 0, raw_data.Length);
 
-            playoutAudioSource.clip.SetData(raw_data, 0);
-            playoutAudioSource.Play();
+                playoutAudioSource.clip.SetData(floatArray2, 0);
+                playoutAudioSource.Play();
+            }
         }
 
         [Command]
@@ -63,13 +71,12 @@ namespace VoiceNetwork
 
         private void StartVoiceRecord()
         {
-            //if (!isMicAvailable) return;
+            if (micAudioSource.clip != null) {
+                float[] data = new float[1024];
+                micAudioSource.clip.GetData(data, 0);
 
-            float[] data = new float[1024];
-            micAudioSource.clip = Microphone.Start("Built-in Microphone", true, 1, 44100);
-            micAudioSource.clip.GetData(data, 0);
-
-            CmsSendAudio(data);
+                CmsSendAudio(data);
+            }
         }
 
         private void Update()
